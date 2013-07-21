045d1b571c24747507e26fbff4584d076a5dcd85
Harsh Gupta <harsh@debian>
2013-07-21 19:30:58 +0530

    solution to exercise 2-27 corrected

--- Chapter2/ex2-27.rkt
+++ Chapter2/ex2-27.rkt
@@ -1,17 +1,37 @@
 #lang planet neil/sicp
 
-(define (reverse items)
-  (define (reverse-iter rev orig)
-    (if (null? orig)
-        rev
-    (reverse-iter (append (list (car orig)) rev) (cdr orig))))
-  (reverse-iter '() items ))
+; list -> bool
+; Determines if a list does not contains lists as values
+(define (not-tree? items)
+  (if (list? (car items))
+      false
+      (if (null? (cdr items))
+          true
+          (not-tree? (cdr items)))))
+(check-expect (not-tree? (list 1)) true)
+(check-expect (not-tree? (list (list 2 3) 1)) false)
 
-(check-expect (reverse '(1 2 3 4)) '(4 3 2 1))
-(check-expect (reverse '('(1 2) '(3 4))) '('(3 4) '(1 2)))
-
+(define ll1 (list 1 2 3 4))
 (define ll2 (list (list 1 2) (list 3 4)))
+(define ll3 (list (list 1 2 (list 3 4)) 5 6))
 
+#;
 (define (deep-reverse items)
   (reverse (map reverse items)))
+; This works only if the list contains lists upto second level
+; and it also passed the test case
+; This shows it is important to test the function extensively and carefully
+
+
+; list -> list
+; returns a list with its elements reversed and with all sublists deep-reversed as wel
+(define (deep-reverse items)
+  (if (list? items)
+      (if (not-tree? items)
+          (reverse items)
+          (reverse (map deep-reverse items)))
+      items))
+
+(check-expect (deep-reverse ll1) (list 4 3 2 1))
 (check-expect (deep-reverse ll2) (list (list 4 3) (list 2 1)))
+(check-expect (deep-reverse ll3) (list 6 5 (list (list 4 3) 2 1)))
